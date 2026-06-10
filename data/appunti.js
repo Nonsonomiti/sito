846ba611f8471034f1c579df111cbef935cd76b0
Nonsonomiti <ferrucciomartino@icloud.com>
2026-06-10 21:05:57 +0200

    ...

--- data/appunti.js
+++ data/appunti.js
@@ -1,3 +1,7 @@
-/* Appunti. Stessa struttura del blog: { date, title, body }
-   data nel formato "AAAA-MM-GG". */
-window.APPUNTI = [];
+window.APPUNTI = [
+  {
+    "date": "2026-06-10",
+    "title": "cartella drive dei miei appunti + roba dell'uni",
+    "body": "non garantisco sia roba buona https://drive.google.com/drive/folders/1GI1awgdyn90yZcGsNq6ydPE7MsBhE1H7?usp=sharing"
+  }
+];
