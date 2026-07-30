30afa687b243596b8763cd572b244dc7481fb969
Martino Cosci <ferrucciomartino@icloud.com>
2026-07-30 19:02:16 +0200

    admin: aggiorna data/blog.js

--- data/blog.js
+++ data/blog.js
@@ -1,24 +1,17 @@
-/* I post del blog. Il piu' recente puoi metterlo dove vuoi:
-   vengono ordinati da soli per data (i nuovi in alto).
-   Ogni post: { date, title, body }   data nel formato "AAAA-MM-GG". */
 window.BLOG = [
-    {
-        date: "2026-05-20",
-        title: "yo",
-        body: "damn"
-    },
-    {
-        date: "2026-06-07",
-        title: "perturbation theory made me feel like this",
-        body: `guys why am i stupid
-what did i do wrong
-i try so hard to live like a normal person yet when i do i am always let down by myself
-i dont want to admit that my life is hard, or awful, but i think it is,
-even when i try not to focus on my illness things keep on falling apart
-good things that happen to me do not bring me joy
-rightfully so people dont know what i am going thru and what i have gone thru and judge as if everything was normal
-i would really like for it to be so and to be able to perform as i wish
-unfortunately i cant and so everywhere i look, in the corners of my life, i am disappointed by my achievements and by my abilities
-i dont know what i should do differently`
-    }
+  {
+    "date": "2026-05-20",
+    "title": "yo",
+    "body": "damn"
+  },
+  {
+    "date": "2026-06-07",
+    "title": "perturbation theory made me feel like this",
+    "body": "guys why am i stupid\nwhat did i do wrong\ni try so hard to live like a normal person yet when i do i am always let down by myself\ni dont want to admit that my life is hard, or awful, but i think it is,\neven when i try not to focus on my illness things keep on falling apart\ngood things that happen to me do not bring me joy\nrightfully so people dont know what i am going thru and what i have gone thru and judge as if everything was normal\ni would really like for it to be so and to be able to perform as i wish\nunfortunately i cant and so everywhere i look, in the corners of my life, i am disappointed by my achievements and by my abilities\ni dont know what i should do differently"
+  },
+  {
+    "date": "2026-07-30",
+    "title": "House S3E12",
+    "body": "- If I die in peace, then I'm just another patient. But if I die suffering --\n- It'll be horrible. Don't do that do either of us.\n- No, I just need to die knowing that something is different because I was here."
+  }
 ];
