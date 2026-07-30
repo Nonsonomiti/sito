214102f50bece22a84c1d8d7f87ad57fd6895358
Martino Cosci <ferrucciomartino@icloud.com>
2026-07-30 19:29:11 +0200

    admin: aggiorna data/blog.js

--- data/blog.js
+++ data/blog.js
@@ -1,10 +1,5 @@
 window.BLOG = [
   {
-    "date": "2026-05-20",
-    "title": "yo",
-    "body": "damn"
-  },
-  {
     "date": "2026-06-07",
     "title": "perturbation theory made me feel like this",
     "body": "guys why am i stupid\nwhat did i do wrong\ni try so hard to live like a normal person yet when i do i am always let down by myself\ni dont want to admit that my life is hard, or awful, but i think it is,\neven when i try not to focus on my illness things keep on falling apart\ngood things that happen to me do not bring me joy\nrightfully so people dont know what i am going thru and what i have gone thru and judge as if everything was normal\ni would really like for it to be so and to be able to perform as i wish\nunfortunately i cant and so everywhere i look, in the corners of my life, i am disappointed by my achievements and by my abilities\ni dont know what i should do differently"
