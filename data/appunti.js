7aeab6a93b6d96d14c9535a7b1abae17b3385aad
Martino Cosci <ferrucciomartino@icloud.com>
2026-07-30 19:08:14 +0200

    admin: aggiorna data/appunti.js

--- data/appunti.js
+++ data/appunti.js
@@ -1,6 +1,6 @@
 window.APPUNTI = [
   {
     "title": "cartella drive dei miei appunti + roba dell'uni",
-    "body": "non garantisco sia roba buona \nhttps://drive.google.com/drive/folders/1ct6xYVl1LSzGmhpaXzPeVjN5v1blOOMX?usp=sharing"
+    "body": "non garantisco sia roba buona https://drive.google.com/drive/folders/1ct6xYVl1LSzGmhpaXzPeVjN5v1blOOMX?usp=sharing"
   }
 ];
